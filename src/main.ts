a1a10a5d5f6dc6d90e2ca28a3b82c6a4af55931b
AdrianIlie01 <ilieadrian01@yahoo.com>
2025-05-06 15:41:04 +0300

    deleted origin

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://ecommerce-frontend-t5lt.onrender.com/',
     exposedHeaders: ['x-forwarded-for'],
+    credentials: true,
   });
 
   await app.listen(process.env.PORT || 3001);

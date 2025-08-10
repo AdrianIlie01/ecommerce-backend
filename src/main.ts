20ac680bc8c5965a473e1ca2e1b58af8a9176845
AdrianIlie01 <ilieadrian01@yahoo.com>
2025-08-10 23:03:41 +0300

    add manually cors origin

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     exposedHeaders: ['x-forwarded-for'],
-    origin: [process.env.ORIGIN],
+    origin: [
+      process.env.ORIGIN,
+      'https://ecommerce-frontend-t5lt.onrender.com',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
     allowedHeaders: [
@@ -17,11 +20,15 @@ async function bootstrap() {
       'Content-Type',
       'Authorization',
       'Origin',
+      'Accept',
     ],
   });
 
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', process.env.ORIGIN);
+    res.header(
+      'Access-Control-Allow-Origin',
+      'https://ecommerce-frontend-t5lt.onrender.com',
+    );
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');

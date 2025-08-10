a838d504b634be8c914e428f9322a5fba09ae073
AdrianIlie01 <ilieadrian01@yahoo.com>
2025-08-10 22:50:36 +0300

    set correct headers

--- src/main.ts
+++ src/main.ts
@@ -15,13 +15,13 @@ async function bootstrap() {
       'Access-Control-Allow-Origin',
       'X-Requested-With',
       'Content-Type',
+      'Authorization',
       'Origin',
     ],
   });
 
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', process.env.ORIGIN);
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');

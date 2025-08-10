3d52527f45697d7f60e248bceca7da0ab6a17a22
AdrianIlie01 <ilieadrian01@yahoo.com>
2025-08-10 22:28:12 +0300

    add frontend origin

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     exposedHeaders: ['x-forwarded-for'],
+    origin: [process.env.ORIGIN],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
   });
 
